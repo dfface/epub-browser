146771195617b134622646d989a68e98fb0ef4b5
Handy <handy1998@qq.com>
2026-03-17 01:44:47 +0800

    fix: some css & height calculate

--- epub_browser/assets/chapter.js
+++ epub_browser/assets/chapter.js
@@ -592,7 +592,7 @@ function initScript() {
 
         const viewportHeight = window.innerHeight;
 
-        let contentHeight = viewportHeight - bottomNavHeight - bottomNavMobileHeight - 5; // 减去边距和安全余量
+        let contentHeight = viewportHeight - bottomNavHeight - bottomNavMobileHeight - 40; // 减去边距和安全余量，安全余量就是 margin-top: 20px，然后上下不就都是 20 了
         contentContainer.style.height = `${viewportHeight - bottomNavHeight}px`;
         
         // 直接设置内容容器的高度
